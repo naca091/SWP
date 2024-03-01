a5457ee5feae20247c799912e196d3cba62df8c4
naca09 <86470016+naca09@users.noreply.github.com>
2024-03-01 10:14:06 +0700

    tien

---
+++ yun-main/yun-main/Project/Project/Project/Models/Customer.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project.Models
+{
+    public class Customer : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
