5a597ebf533b403502dc8afd893ac4924c5eaafc
naca091 <161755174+naca091@users.noreply.github.com>
2024-03-05 20:38:05 +0700

    Update HomeController.cs

--- yun-main/yun-main/Project/Project/Project/Controllers/HomeController.cs
+++ yun-main/yun-main/Project/Project/Project/Controllers/HomeController.cs
@@ -1,24 +1,53 @@
-﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Project.Data;
 using Project.Models;
 using System.Diagnostics;
 
 namespace Project.Controllers
 {
-/*    [Authorize(Roles = "Admin")]
-*/    public class HomeController : Controller
+    /*    [Authorize(Roles = "Admin")]
+    */
+    public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
+
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            // Lấy Tổng doanh thu theo tháng
+            decimal totalEarnings = _context.Revenue.Sum(r => r.Earnings);
+            ViewBag.TotalEarnings = totalEarnings;
+
+            // Số lượng Note đang chờ duyệt
+            int quantityToAdd = _context.Notes.Count(n => n.Status == 2);
+            ViewBag.Quantity = quantityToAdd;
+
+            // Tính tổng tất cả các Total từ Note
+            int currentYear = DateTime.Now.Year;
+            decimal totalFromNotes = await _context.Notes
+                .Where(n => n.CreatedDate.Year == currentYear)
+                .SumAsync(n => n.Total);
+            ViewBag.TotalFromNotes = totalFromNotes.ToString("N0");
+
+            // Đếm tổng số Customer
+            int customerCount = await _context.Customers.CountAsync();
+            ViewBag.CustomerCount = customerCount;
+
             return View();
         }
+
+
+
 
         public IActionResult Privacy()
         {
